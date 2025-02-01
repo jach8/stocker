31b81ecb090bb34f0f3a517581d22409ce0c7da1
Jerald Achaibar <jeraldthatman@gmail.com>
2025-02-01 17:12:37 -0500

    code refresh and refactoring

--- bin/options/optgd/db_connect.py
+++ bin/options/optgd/db_connect.py
@@ -1,15 +1,8 @@
-""" 
-Connect to all databases 
-    - Option database 
-    - Option Statistics Database
-    - Tickers 
-    - Option Change DB ** This is updated, tracking the change in each individual contracts. 
-    
-Contains some utility functions for: 
-    1. Inserting New Data into the database, 
-    2. Checking for the stock in the option database
-    3. Getting the last 5 days of option data for a stock. ** To be used in tracking the change in each contract. 
-    4. Getting the last dates for each day for a stock in the db. ** To be used in tracking the change in each contract.
+"""
+
+Do we really need to connect to ALL the databases at once? 
+- Should we connect to the databases as needed?
+
 
 """
 
@@ -21,24 +14,80 @@ import datetime as dt
 import re
 import time
 import json 
-import os 
-
-def check_connection(path):
-    if os.path.exists(path):
-        return True
-    else:
-        return False
 
 class Connector: 
-    """ 
-    Database Connector for Options Data
-    """
     def __init__(self, connections):
+        """ 
+        Database Connector for Options Data. 
+        
+        
+        Args:
+            connections (dict): Dictionary of the paths to the databases. 
+                Example:
+                    {
+                        'option_db': 'Path to the option database',
+                        'change_db': 'Path to the option change database',
+                        'vol_db': 'Path to the volume database',
+                        'stats_db': 'Path to the statistics database',
+                        'tracking_db': 'Path to the tracking database',
+                        'tracking_values_db': 'Path to the tracking values database',
+                        'backup_db': 'Path to the backup database',
+                        'inactive_db': 'Path to the inactive database',
+                        'ticker_path': 'Path to the ticker json file
+                    }
+                    
+        Attributes:
+            stocks: Dictionary of the stocks.
+            all_stocks: List of all the stocks in the database.
+            
+        To Do: 
+            1_ Change the way we connect to the databases. Opting for a Pooling Method, 
+                the main class that manages connections should look something like this:
+                    Example: 
+                    
+                    # connection_manager.py
+                    import sqlite3
+                    from contextlib import contextmanager
+
+                    class ConnectionManager:
+                        def __init__(self, db_paths):
+                            self.db_paths = db_paths
+                            self.connections = {}
+
+                        @contextmanager
+                        def get_connection(self, db_name):
+                            if db_name not in self.connections:
+                                self.connections[db_name] = sqlite3.connect(self.db_paths[db_name])
+                            try:
+                                yield self.connections[db_name]
+                            finally:
+                                # Here you might choose to not close the connection if using lazy loading
+                                # self.connections[db_name].close()
+                                pass
+
+                        def close_all(self):
+                            for conn in self.connections.values():
+                                conn.close()
+                            self.connections.clear()
+
+                    # Usage in other modules:
+                    # from connection_manager import ConnectionManager
+                    # conn_manager = ConnectionManager({'option_db': 'path_to_db'})
+                    # with conn_manager.get_connection('option_db') as conn:
+                    #     cursor = conn.cursor()
+                    #     # Operations here
+                    
+            2_ Add method to Ensure that Deletions from the Option DB are correctly Logged in the backup DB or Inactive DB.
+                - For any LARGE CHANGES you should log them in the backup db or inactive db.
+                (1/31/2025): Error: I mistakenly deleted the entire option_db and change_db for SPY :(
+                    - This happened because i used 'replace' instead of 'append' when updating the option_db.
+                    
+        """
         self.execution_start_time = time.time()
         try:
             # Add stocks, Keys: ['all_stocks', 'bonds','etf', 'equities', 'market', 'mag8']
             self.stocks = json.load(open(connections['ticker_path'], 'r'))     
-            self.all_stocks = self.stocks['all_stocks']
+            self.all_stocks = self.stocks['market'] + self.stocks['bonds'] + self.stocks['mag8'] + self.stocks['etf'] + self.stocks['equities']
             
             # Add Connections 
             self.path_dict = connections
@@ -64,24 +113,46 @@ class Connector:
             print("Options db Connected: {}".format(dt.datetime.now()))
         
         except Exception as e:
-            print("Error: ", e)
-            # Print the last defined variable
-            print(self.__dict__)
-            raise Exception("Error: ", e)
-
+            print("Connection Failed: ", e,)       
         
+    def __check_inactive_db_for_stock(self, stock: str) -> bool: 
+        """ 
+        Check if the stock is in the inactive database 
         
+        Args:
+            stock (str): Stock Ticker Symbol
         
-    def _purge_inactive(self, stock):
+        Returns:
+            bool: True if the stock is in the database, False if not.
+        
+        """
+        query = f"""
+        select exists(select 1 from sqlite_master where type='table' and name='{stock}')
+        """
+        cursor = self.inactive_db_cursor
+        valid = cursor.execute(query).fetchone()[0]
+        return bool(valid)
+            
+    def __purge_inactive(self, stock: str) -> None:
         """
         Purge Inactive Contracts from the Option_db database. 
             - Save them in the inactive_db so that we can use them for tracking. 
+        
+        Args:
+            stock (str): Stock Ticker Symbol
+        
+        Returns:
+            None
+        
         """
-        exp_q = f''' select * from "{stock}" where date(expiry) < date('now') '''
-        exp = pd.read_sql_query(exp_q, self.option_db)
+        exp_q = f''' select * from {stock} where date(expiry) < date('now') '''
+        cursor = self.write_option_db_cursor
+        exp = cursor.execute(exp_q).fetchall()
+        exp = pd.DataFrame(exp, columns = [x[0] for x in cursor.description])
+        
         if exp.shape[0] > 0:
             contracts = ','.join([f'"{x}"' for x in exp.contractsymbol])
-            change_db_q = f''' select * from "{stock}" where contractsymbol in ({contracts}) '''
+            change_db_q = f''' select * from {stock} where contractsymbol in ({contracts}) '''
             cdb = pd.read_sql_query(change_db_q, self.change_db)
             
             if self._check_inactive_db_for_stock(stock):
@@ -94,159 +165,45 @@ class Connector:
                 cdb.to_sql(stock + "_change", self.inactive_db, if_exists='replace', index=False)
             
             # Purge from Option DB 
-            self.write_option_db_cursor.execute(f'delete from "{stock}" where date(expiry) < date("now")')
+            self.write_option_db_cursor.execute(f'delete from {stock} where date(expiry) < date("now")')
             self.write_option_db.commit()
             
             # Purge from Change DB
-            self.change_db_cursor.execute(f'delete from "{stock}" where contractsymbol in ({contracts})')
+            self.change_db_cursor.execute(f'delete from {stock} where contractsymbol in ({contracts})')
             self.change_db.commit()
         
-    def close_connections(self):
-        for i in [self.option_db, self.write_option_db, self.change_db, self.vol_db, self.stats_db, self.tracking_db, self.tracking_values_db, self.backup]:
+    def close_connections(self) -> None:
+        """
+        Closes all the connections to the databases. 
+        
+        Returns:
+            None
+        
+        """
+        db_list = [
+            self.option_db, 
+            self.write_option_db, 
+            self.change_db, 
+            self.vol_db, 
+            self.stats_db, 
+            self.tracking_db, 
+            self.tracking_values_db, 
+            self.backup
+        ]
+        for i in db_list:
             i.close()
         end_time = time.time()
         runtime_min = (end_time - self.execution_start_time) / 60
         print("Connections Closed {}\nTotal Runtime: {:.2f} min".format(dt.datetime.now(), runtime_min))
         print()
         return None
-
-    def clear_tables(self):
-        ''' Save a log and clear the Unusual Activity, exp move, and high percentage tables. '''
-        # save backup file 
-        self.stats_db.backup(self.backup)
-        c = self.stats_db.cursor()
-        tables = [i[0] for i in c.execute("SELECT name FROM sqlite_master WHERE type='table';").fetchall()]
-        for i in tables:
-            c.execute(f'drop table {i}')
-            self.stats_db.commit()
-        return None
     
-    def _check_for_stock_in_option_db(self, stock):
-        """ Check if the stock is in the option database """
-        check = self.option_db_cursor.execute(f'select name from sqlite_master where type = "table" and name = "{stock}"').fetchall()
-        if check != []:
-            return False
-        else:
-            return True
-        
-    def _check_for_stock_in_change_db(self, stock):
-        """ Check if the stock is in the change database """
-        check = self.change_db_cursor.execute(f'select name from sqlite_master where type = "table" and name = "{stock}"').fetchall()
-        if check != []:
-            return False
-        else:
-            return True
-    
-    def _check_tracking_chain_for_stock(self, stock):
-        """ Check if the stock has an existing table in the tracking db and if the table is empty
-            Returns:
-                - True if the stock is in the tracking db 
-                - False if the stock is not in the tracking db
-        """
-        q = f'select name from sqlite_master where type="table" and name="{stock}"'
-        cursor = self.tracking_db.cursor()
-        res = cursor.execute(q).fetchall()
-        if res != []:
-            return True
-        else:
-            return False
-    
-    def _check_tracking_values_for_stock(self, stock):
-        """ Read in the Contracts we are tracking
-            Returns:
-                - False if the stock is not in the tracking values db 
-                - True if the stock is in the tracking values db
-        """
-        valid_tickers = pd.read_sql('select * from sqlite_master where type="table"', self.tracking_values_db)
-        if stock not in valid_tickers.name.values:
-            return False
-        else:
-            q = f'select count(*) from "{stock}"'
-            if self.tracking_values_db.execute(q).fetchone()[0] == 0:
-                return False
-            else:
-                return True
-        
-    def _check_inactive_db_for_stock(self, stock): 
-        """ Check if the stock is in the inactive database """
-        valid = pd.read_sql('select * from sqlite_master where type="table"', self.inactive_db)
-        if stock in valid.name.values:
-            return True
-        else:
-            return False
-        
-    
-    def _recent_option_chain(self, stock):
-        """ Return the last 5 days of option data """
-        if self._check_for_stock_in_option_db(stock):
-            print(f'${stock.upper()} not in the database')
-            return None
-        else:
-            dates = self._last_dates(stock)
-            dates = ','.join([f'"{x}"' for x in dates])
-            q = f''' select * from "{stock}" where datetime(gatherdate) in ({dates}) '''
-            return pd.read_sql_query(q, self.option_db)
-    
-    
-    def _last_dates(self, stock, N = 5):
-        """ Return the last dates for each day for a stock in the db """
-        q = f'''
-        select 
-            distinct
-            last_value (datetime(gatherdate)) over 
-                (partition by date(gatherdate) rows between 
-                unbounded preceding and unbounded following) 
-            as gatherdate
-        from "{stock}"
-            where date(gatherdate) > date("2022-11-15")
-        '''
-        cursor = self.option_db.cursor()
-        out = cursor.execute(q).fetchall()
-        out = [x[0] for x in out]
-        return out[-N:]
-    
-    def _max_dates(self, stock):
-        ''' Returns the max date in the database '''
-        q0 = f'''
-            select
-            date(gatherdate) as gatherdate,
-            max(datetime(gatherdate)) as maxdate
-            from "{stock}"
-            group by date(gatherdate)
-        '''
-        df0 = pd.read_sql_query(q0, self.option_db)
-        return ','.join([f"'{x}'" for x in df0['maxdate']])
-
-    def describe_option(self, y):
-        ''' Given an option contract symbol, using regular expressions to return the stock, expiration date, contract type, and strike price. '''
-        valid = re.compile(r'(?P<stock>[A-Z]+)(?P<expiry>[0-9]+)(?P<type>[C|P])(?P<strike>[0-9]+)')
-        stock = valid.match(y).group('stock')
-        expiration = dt.datetime.strptime(valid.match(y).group('expiry'), "%y%m%d")
-        conttype = valid.match(y).group('type')
-        strike = float(valid.match(y).group('strike')) / 1000
-        return ("$"+stock, conttype, float(strike), expiration.strftime('%m/%d/%y'))
-    
-        
-        
-        
 if __name__ == "__main__":
     print("True Humility is not thinking less of yourself; It is thinking of yourself less.")
-
-    connections = {
-            'inactive_db': 'bin/pipe/log/inactive.db',
-            'backup_db': 'bin/pipe/log/backup.db',
-            'tracking_values_db': 'data/options/bin/pipe/test_data/tracking_values.db',
-            'tracking_db': 'data/options/bin/pipe/test_data/tracking.db',
-            'stats_db': 'bin/pipe/test_data/stats.db',
-            'vol_db': 'bin/pipe/test_data/vol.db',
-            'change_db': 'bin//pipe/test_data/option_change.db', 
-            'option_db': 'bin/pipe/test_data/test.db', 
-            'testing_option_db': 'bin/pipe/test_data/test.db',
-            'options_stat': 'bin/pipe/test_data/options_stat.db',
-            'ticker_path': 'data/stocks/tickers.json'
-            }    
+    import sys
+    sys.path.append('/Users/jerald/Documents/Dir/Python/Stocks')
+    from bin.main import get_path 
+    connections = get_path()  
     conn = Connector(connections)
-    conn._purge_inactive('spy')
-    conn.close_connections()        
-            
-            
+    # conn._purge_inactive('spy')
+    # conn.close_connections()        
